fd65e66c96a02d7a673458c266c74d36d85ea9d1
Florian Kempenich <Flori@nKempenich.com>
2020-05-27 16:06:40 +0100

    Add some basic logic

--- appToTest/routes/index.js
+++ appToTest/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/greeting", (req, res) => {
+  const name = req.body.name;
+  res.render("greeting", {
+    title: "Greeting",
+    name,
+  });
 });
 
 module.exports = router;
